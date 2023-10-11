314ff476795f4370470040b98ed2decfcc6086e7
scottdaco <119343979+scottdaco@users.noreply.github.com>
2023-10-11 14:52:37 -0500

    add site

---
+++ models/lizard.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+
+public class Lizard
+{
+    public int Id { get; set; }
+    public string? name { get; set; }
+    public string? region { get; set; }
+    public decimal length { get; set; }
+}
